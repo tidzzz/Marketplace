a8f82a0f92508a6d2d3b995be32277998b48a189
“a24ithay” <abilan.ithayakumar@imt-atlantique.net>
2025-11-16 16:42:10 +0100

    models alone

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, jsonify, request, render_template
 from database.database import db, init_database
+from database.models import *
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
@@ -26,45 +27,6 @@ def admin_required(f):
             return jsonify({"error": "Forbidden: admin only"}), 403
         return f(*args, **kwargs)
     return decorated_function
-
-
-# DÉFINITION DU MODÈLE USER
-#    Cette classe hérite de db.Model. SQLAlchemy sait alors
-#    qu'elle correspond à une table.
-class User(db.Model): # type: ignore
-    # __tablename__ est optionnel, mais c'est une bonne pratique de le nommer explicitement.
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-    credits_cents = db.Column(db.Integer, nullable=False, default=0)
-
-    
-    def __repr__(self):
-        return f'<User {self.email}>'
-    
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    
-    
-    parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
-
-    def to_dict(self):
-        
-        return {
-            "id": self.id,
-            "name": self.name,
-            "parent_id": self.parent_id
-        }
-
-    def __repr__(self):
-        return f'<Category {self.name}>'
 
 
 @app.route('/api/categories', methods=['GET'])
@@ -148,4 +110,3 @@ def test():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
--- database/models.py
+++ database/models.py
@@ -1,6 +1,41 @@
 from database.database import db
 
-""" class Example(db.Model):
+
+# DÉFINITION DU MODÈLE USER
+#    Cette classe hérite de db.Model. SQLAlchemy sait alors
+#    qu'elle correspond à une table.
+class User(db.Model): # type: ignore
+    # __tablename__ est optionnel, mais c'est une bonne pratique de le nommer explicitement.
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.Text)
-    isDone = db.Column(db.Boolean) """
+    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    credits_cents = db.Column(db.Integer, nullable=False, default=0)
+
+    
+    def __repr__(self):
+        return f'<User {self.email}>'
+    
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    
+    
+    parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
+
+    def to_dict(self):
+        
+        return {
+            "id": self.id,
+            "name": self.name,
+            "parent_id": self.parent_id
+        }
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
